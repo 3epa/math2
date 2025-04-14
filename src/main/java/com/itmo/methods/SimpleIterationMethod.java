0de36bdd9ab0f4d3bb5f6df0a34d388201b3fd93
3epa <egorfelix@yandex.ru>
2025-04-15 00:01:50 +0300

    SimpleIterationMethod first version

---
+++ src/main/java/com/itmo/methods/SimpleIterationMethod.java
@@ -0,0 +1,50 @@
+package com.itmo.methods;
+
+import java.util.function.Function;
+
+public class SimpleIterationMethod extends Method {
+    private final Function<Double, Double> df;
+
+    public SimpleIterationMethod(double epsilon, Function<Double, Double> f, Function<Double, Double> df) {
+        super(epsilon, f);
+        this.df = df;
+    }
+
+    private double findLipschitzCoefficient(double a, double b) {
+        double max = 0;
+        for (double x = a; x < b; x += epsilon) {
+            max = Math.max(max, Math.abs(df.apply(x)));
+        }
+        return max;
+    }
+
+    public double phi(double a, double b, double x) {
+        return x - checkSignConsistency(df, a, b) / findLipschitzCoefficient(a, b) * f.apply(x);
+    }
+
+    @Override
+    public double solve(double a, double b) {
+        double x = a;
+        double xPrev;
+
+        do {
+            xPrev = x;
+            x = phi(a, b, xPrev);
+        } while (!isSolved(x,xPrev));
+        return x;
+    }
+
+
+    private double checkSignConsistency(Function<Double, Double> function, double a, double b) {
+        double firstSign = Math.signum(function.apply(a));
+
+        for (double x = a; x < b; x += epsilon) {
+            double currentSign = Math.signum(function.apply(x));
+
+            if (currentSign != firstSign) {
+                return 0;
+            }
+        }
+        return firstSign;
+    }
+}
