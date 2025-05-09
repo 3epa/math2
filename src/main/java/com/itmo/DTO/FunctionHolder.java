6dc43d2f0fa8310e67cd127486b02954f0d7b965
3epa <egorfelix@yandex.ru>
2025-05-09 18:42:56 +0300

    add description field in FunctionHolder

--- src/main/java/com/itmo/DTO/FunctionHolder.java
+++ src/main/java/com/itmo/DTO/FunctionHolder.java
@@ -3,13 +3,16 @@ package com.itmo.DTO;
 import java.util.function.Function;
 
 public class FunctionHolder {
+    private final String description;
     private final Function<Double, Double> f;
     private final Function<Double, Double> df;
     private final Function<Double, Double> d2f;
 
-    public FunctionHolder(Function<Double, Double> f,
+    public FunctionHolder(String description,
+                          Function<Double, Double> f,
                           Function<Double, Double> df,
                           Function<Double, Double> d2f) {
+        this.description = description;
         this.f = f;
         this.df = df;
         this.d2f = d2f;
@@ -25,5 +28,9 @@ public class FunctionHolder {
 
     public Function<Double, Double> getD2f() {
         return d2f;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
